5ed8fc868cda69623e9f555479b039b0e17c3b61
Gabriel Oliveira <gogvale2011@gmail.com>
2020-01-11 10:26:38 -0600

    Placeholder JSON API

--- src/App.js
+++ src/App.js
@@ -3,10 +3,30 @@ import './App.css';
 
 class App extends React.Component {
   constructor() {
-    super(props);
-    state = {
-      monster: [],
+    super();
+    this.state = {
+      monsters: [],
     };
+  }
+  render() {
+    return (
+      <div className='App'>{
+        this.state.monsters.map(
+          monster => (
+            <h1 key={monster.id}>
+              {monster.name}
+            </h1>
+          )
+        )
+      }
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
   }
 }
 
