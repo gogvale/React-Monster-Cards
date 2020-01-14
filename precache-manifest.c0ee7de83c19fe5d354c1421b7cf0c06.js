c2bff4399ffa6e878eec2b4b1a1e19af5000780b
Gabriel Oliveira <gogvale2011@gmail.com>
2020-01-13 21:45:37 -0600

    Updates

---
+++ precache-manifest.c0ee7de83c19fe5d354c1421b7cf0c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5521c289e9673e81addd55b487d9d8",
+    "url": "/React-Monster-Cards/index.html"
+  },
+  {
+    "revision": "f617b70c8d3c362f27f9",
+    "url": "/React-Monster-Cards/static/css/main.c10ad147.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/React-Monster-Cards/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Monster-Cards/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f617b70c8d3c362f27f9",
+    "url": "/React-Monster-Cards/static/js/main.82a9a755.chunk.js"
+  },
+  {
+    "revision": "242712bcf92476ff0ad8",
+    "url": "/React-Monster-Cards/static/js/runtime-main.5643bc65.js"
+  }
+]);
