ec2a49082328403df0d84fee0158202f3b7fb4a1
Gabriel Oliveira <gogvale2011@gmail.com>
2020-01-13 21:22:56 -0600

    Add image and email to monster

--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -4,6 +4,8 @@ import './card.styles.css';
 export const Card = (props) =>
     (
         <div className='card-container'>
-            <h1>{props.monster.name}</h1>
+            <img alt="monster-pic" src={`https://robohash.org/${props.monster.id}.png?size=180x180`}></img>
+            <h2>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
         </div>
     );
